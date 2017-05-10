5ea3355f0b62f71306d7b3ce28bcf9d11ac7ef42
Shay Moshe <shymoshe@gmail.com>
2017-05-10 08:00:28 +0300

    custom team

--- client/main.jsx
+++ client/main.jsx
@@ -3,12 +3,19 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { Router, Route, Link, hasHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import '../imports/startup/accounts-config';
 import App from '../imports/ui/App';
 
+
 Meteor.startup(() => {
-  render(<MuiThemeProvider>
+  const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: '#00abe6'
+    },
+});
+  render(<MuiThemeProvider muiTheme={muiTheme}>
     <App />
   </MuiThemeProvider>,
     document.getElementById('render-target'));
